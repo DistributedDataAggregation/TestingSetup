91a4a74edc4e62d289236d77a9387da3daf1fdff
Kapiszon <krol.kacper.pg2@gmail.com>
2025-01-04 14:29:47 +0100

    feat: fix tests structure

--- performence_tests/performence_test.py
+++ performence_tests/performence_test.py
@@ -1,152 +1,115 @@
 import pytest
 import requests
 import math
-from test_quries import Medium_SmallKeys_Test1, Small_MediumKeys_Test1
+from test_queries import tests
 from tabulate import tabulate
+import json
 
-
-# Adres endpointu API
+# Adres endpointu API, docker i local
 # API_URL = "http://localhost:80/api/v1/query"
 API_URL = "http://localhost:3000/api/v1/query"
 
 NUMBER_OF_REPETITIONS = 5
+ENABLE_AVERAGE_TEST = False
 
-results=[]
+results = []
 
+@pytest.mark.parametrize("test", tests)
+def test_performance_one(test):
+    """Testuje czas odpowiedzi dla różnych zapytań (pojedyncze wykonanie)."""
 
+    print(f"Table name: {test['testname']}")
 
-@pytest.mark.parametrize("query", Medium_SmallKeys_Test1)
-def test_performence_once(query):
-    """Testuje, czas odpowiedzi dla róznych zapytań"""
+    for query in test["queries"]:
+        try:
+            print(f"Test name: {query['name']}")
+            response = requests.post(API_URL, json=query["json"])
+            assert response.status_code == 200, f"Expected HTTP 200, got {response.status_code}"
+            response_json = response.json()
+            
+            response_time = response.elapsed.total_seconds()
+            response_time_from_response = float(response_json["processing_time"]) / 1000
+            results.append((query["name"], response_time, response_time_from_response))
 
-    response = requests.post(API_URL, json=query["json"])
-    assert response.status_code == 200, f"Expected HTTP 200, got {response.status_code}"
-    response_json = response.json()
-    response_time = response.elapsed.total_seconds()
-    response_time_from_response = float(response_json["processing_time"]) / 1000
-    results.append((query["name"], response_time, response_time_from_response))
+            # Sprawdzenie struktury odpowiedzi
+            assert "result" in response_json, "Key 'result' missing in response."
+            assert "values" in response_json["result"], "Key 'values' missing in 'result'."
+            for value in response_json["result"]["values"]:
+                assert (
+                    "grouping_value" in value
+                ), "'grouping_value' missing in one of the values."
+                assert "results" in value, "'results' missing in one of the values."
+                for result in value["results"]:
+                    assert "value" in result, "'value' missing in one of the results."
 
-    # Sprawdzenie struktury odpowiedzi
-    assert "result" in response_json, "Key 'result' missing in response."
-    assert "values" in response_json["result"], "Key 'values' missing in 'result'."
-    for value in response_json["result"]["values"]:
-        assert (
-            "grouping_value" in value
-        ), "'grouping_value' missing in one of the values."
-        assert "results" in value, "'results' missing in one of the values."
-        for result in value["results"]:
-            assert "value" in result, "'value' missing in one of the results."
+            print("PASSED")
+        except Exception as e:
+            print(f"FAILED: {query['name']} - {e}")
+            continue
 
-
-
-def test_display_results():
     print("\n")
-    print(tabulate(results, headers=["Nazwa zapytania", "Czas odpowiedzi (s)"]))
+    print(tabulate(results, headers=["Nazwa zapytania", "Czas odpowiedzi (s)", "Czas przetwarzania z odpowiedzi (s)"]))
     print("\n")
+    results.clear()
 
 def calculate_standard_deviation(times, mean):
+    """Oblicza odchylenie standardowe."""
     variance = sum((time - mean) ** 2 for time in times) / len(times)
     return math.sqrt(variance)
 
+@pytest.mark.parametrize("test", tests)
+def test_performance_average(test):
+    """Testuje czas odpowiedzi dla różnych zapytań (wielokrotne wykonanie)."""
+    if not ENABLE_AVERAGE_TEST:
+        pytest.skip("Test średnich czasów wylaczony!")
 
-@pytest.mark.parametrize("query", Medium_SmallKeys_Test1)
-def test_performance_average(query):
-    """Testuje czas odpowiedzi dla różnych zapytań, wykonując je wielokrotnie, obliczając średni czas i odchylenie standardowe."""
-    response_times = []
-    processing_times = []
-    results=[]
+    print(f"Table name: {test['testname']}")
 
-    for _ in range(NUMBER_OF_REPETITIONS):
-        response = requests.post(API_URL, json=query["json"])
-        assert response.status_code == 200, f"Expected HTTP 200, got {response.status_code}"
-        response_json = response.json()
+    for query in test["queries"]:
+        print(f"Test name: {query['name']}")
+        response_times = []
+        processing_times = []
 
-        response_times.append(response.elapsed.total_seconds())
-        processing_times.append(float(response_json["processing_time"]) / 1000)
+        for _ in range(NUMBER_OF_REPETITIONS):
+            response = requests.post(API_URL, json=query["json"])
+            assert response.status_code == 200, f"Expected HTTP 200, got {response.status_code}"
+            response_json = response.json()
 
-        # Sprawdzenie struktury odpowiedzi
-        assert "result" in response_json, "Key 'result' missing in response."
-        assert "values" in response_json["result"], "Key 'values' missing in 'result'."
-        for value in response_json["result"]["values"]:
-            assert "grouping_value" in value, "'grouping_value' missing in one of the values."
-            assert "results" in value, "'results' missing in one of the values."
-            for result in value["results"]:
-                assert "value" in result, "'value' missing in one of the results."
+            response_times.append(response.elapsed.total_seconds())
+            processing_times.append(float(response_json["processing_time"]) / 1000)
 
-    avg_response_time = sum(response_times) / NUMBER_OF_REPETITIONS
-    avg_processing_time = sum(processing_times) / NUMBER_OF_REPETITIONS
-    stddev_response_time = calculate_standard_deviation(response_times, avg_response_time)
-    stddev_processing_time = calculate_standard_deviation(processing_times, avg_processing_time)
+            # Sprawdzenie struktury odpowiedzi
+            assert "result" in response_json, "Key 'result' missing in response."
+            assert "values" in response_json["result"], "Key 'values' missing in 'result'."
+            for value in response_json["result"]["values"]:
+                assert "grouping_value" in value, "'grouping_value' missing in one of the values."
+                assert "results" in value, "'results' missing in one of the values."
+                for result in value["results"]:
+                    assert "value" in result, "'value' missing in one of the results."
 
-    results.append((
-        query["name"],
-        avg_response_time,
-        stddev_response_time,
-        avg_processing_time,
-        stddev_processing_time,
-    ))
+        avg_response_time = sum(response_times) / NUMBER_OF_REPETITIONS
+        avg_processing_time = sum(processing_times) / NUMBER_OF_REPETITIONS
+        stddev_response_time = calculate_standard_deviation(response_times, avg_response_time)
+        stddev_processing_time = calculate_standard_deviation(processing_times, avg_processing_time)
 
-def test_display_results():
-    """Wyświetla wyniki testów w tabeli."""
+        results.append((
+            query["name"],
+            avg_response_time,
+            stddev_response_time,
+            avg_processing_time,
+            stddev_processing_time,
+        ))
+
     print("\n")
     print(tabulate(
         results,
         headers=[
-            "Nazwa zapytania", 
-            "Średni czas odpowiedzi (s)", 
-            "Odchylenie standardowe odpowiedzi (s)", 
-            "Średni czas przetwarzania (s)", 
+            "Nazwa zapytania",
+            "Średni czas odpowiedzi (s)",
+            "Odchylenie standardowe odpowiedzi (s)",
+            "Średni czas przetwarzania (s)",
             "Odchylenie standardowe przetwarzania (s)"
         ]
     ))
     print("\n")
-
-
-
-
-
-@pytest.mark.integration
-def test_more_parallel_requests():
-    """Testuje, czy rwiele równoległych zapytań zwraca poprawne odpowiedzi."""
-    query_payload = {
-        "group_columns": ["Surname"],
-        "select": [
-            {"column": "Cube Numbers", "function": "Maximum"},
-            {"column": "Age", "function": "Minimum"},
-        ],
-        "table_name": "test",
-    }
-
-    # Wysłanie zapytań równolegle
-    response_1 = requests.post(API_URL, json=query_payload)
-    response_2 = requests.post(API_URL, json=query_payload)
-    response_3 = requests.post(API_URL, json=query_payload)
-    response_4 = requests.post(API_URL, json=query_payload)
-
-    # Sprawdzenie odpowiedzi dla pierwszego zapytania
-    assert (
-        response_1.status_code == 200
-    ), f"Expected HTTP 200 for query 1, got {response_1.status_code}"
-    response_json_1 = response_1.json()
-    assert "result" in response_json_1, "Key 'result' missing in response for query 1."
-
-    # Sprawdzenie odpowiedzi dla drugiego zapytania
-    assert (
-        response_2.status_code == 200
-    ), f"Expected HTTP 200 for query 2, got {response_2.status_code}"
-    response_json_2 = response_2.json()
-    assert "result" in response_json_2, "Key 'result' missing in response for query 2."
-
-    # Sprawdzenie odpowiedzi dla trzeciego zapytania
-    assert (
-        response_3.status_code == 200
-    ), f"Expected HTTP 200 for query 3, got {response_3.status_code}"
-    response_json_3 = response_3.json()
-    assert "result" in response_json_3, "Key 'result' missing in response for query 3."
-
-    # Sprawdzenie odpowiedzi dla czwartego zapytania
-    assert (
-        response_4.status_code == 200
-    ), f"Expected HTTP 200 for query 4, got {response_4.status_code}"
-    response_json_4 = response_4.json()
-    assert "result" in response_json_4, "Key 'result' missing in response for query 4."
+    results.clear()
---
+++ performence_tests/test_queries.py
@@ -0,0 +1,318 @@
+tests= [
+    {
+        "testname": "Small_MediumKeys_Test1",
+        "queries": [
+         {
+                "name": "Simple query 1",
+                "json": {
+                    "group_columns": ["Surname"],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Age", "function": "Minimum"},
+                        {"column": "Age", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Simple query 2",
+                "json": {
+                    "group_columns": ["Units of Time", "Surname"],
+                    "select": [
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Age", "function": "Maximum"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Medium query 1",
+                "json": {
+                    "group_columns": ["Tree Names", "Company Departments"],
+                    "select": [
+                        {"column": "Prime Numbers", "function": "Minimum"},
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Fibonacci Numbers", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Medium query 2",
+                "json": {
+                    "group_columns": ["Tree Names", "Company Departments"],
+                    "select": [
+                        {"column": "Prime Numbers", "function": "Minimum"},
+                        {"column": "Cube Numbers", "function": "Maximum"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Complex query 1",
+                "json": {
+                    "group_columns": ["Colors", "Eye Colors", "Sports","Car Brands", "Zodiac Signs"],
+                    "select": [
+                        {"column": "Age", "function": "Average"},
+                        {"column": "Fibonacci Numbers", "function": "Maximum"},
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Kraje", "function": "Maximum"},
+                        {"column": "Days of the Month", "function": "Minimum"},
+                        {"column": "Negative Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+          
+            {
+                "name": "Complex query 2",
+                "json": {
+                    "group_columns": ["Colors", "Eye Colors", "Sports", "Tree Names", "Company Departments", "Car Brands", "Zodiac Signs", "Web Browsers", "Senses", "Letters"],
+                    "select": [
+                        {"column": "Age", "function": "Average"},
+                        {"column": "Fibonacci Numbers", "function": "Maximum"},
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Odd Numbers", "function": "Average"},
+                        {"column": "Shoe Sizes", "function": "Maximum"},
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Maximum"},
+                        {"column": "Days of the Month", "function": "Minimum"},
+                        {"column": "Negative Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Complex query 3",
+                "json": {
+                    "group_columns": [
+                        "Surname", "Age", "Colors", "Sports", "Eye Colors", "Tree Names", "Company Departments", "Car Brands", "Rainbow Colors", "Days of the Month", 
+                        "Shoe Sizes", "Flowers", "Zodiac Signs", "Types of Fruits", "Web Browsers", "Cat Breeds", "Computer Brands", "Senses", "Letters", "Animals"
+                    ],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Fibonacci Numbers", "function": "Average"},
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Odd Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Minimum"},
+                        {"column": "Shoe Sizes", "function": "Maximum"},
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Average"},
+                        {"column": "Negative Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            },
+            {
+                "name": "Extreme complex query 1",
+                "json": {
+                    "group_columns": [
+                        "Surname", "Age", "Colors", "Sports", "Eye Colors", "Tree Names", "Company Departments", "Car Brands", "Rainbow Colors", "Days of the Month", 
+                        "Shoe Sizes", "Flowers", "Zodiac Signs", "Types of Fruits", "Web Browsers", "Cat Breeds", "Computer Brands", "Senses", "Letters", "Animals",
+                        "Body Parts", "Mountains", "Grades", "Roles", "Logical Operators", "Payment Methods", "Vegetables", "Animals", "Prime Numbers", "Cube Numbers"
+                    ],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Fibonacci Numbers", "function": "Average"},
+                        {"column": "Cube Numbers", "function": "Maximum"},
+                        {"column": "Odd Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Minimum"},
+                        {"column": "Shoe Sizes", "function": "Maximum"},
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Average"},
+                        {"column": "Negative Numbers", "function": "Average"},
+                        {"column": "Prime Numbers", "function": "Average"}
+                    ],
+                    "table_name": "small_size_some_keys_github_actions"
+                }
+            }
+        ]
+    },
+    {
+        "testname": "Medium_SmallKeys_Test1",
+        "queries": [
+            {
+                "name": "Simple query 1",
+                "json": {
+                    "group_columns": ["Position"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Simple query 2",
+                "json": {
+                    "group_columns": ["Position"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"},
+                        {"column": "Age", "function": "Minimum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Simple query 3",
+                "json": {
+                    "group_columns": ["Position"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Medium",
+                "json": {
+                    "group_columns": ["Position"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"},
+                        {"column": "Age", "function": "Minimum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Parallel query 5",
+                "json": {
+                    "group_columns": ["Position"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Medium query 1",
+                "json": {
+                    "group_columns": ["Position", "Surname", "Age"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"},
+                        {"column": "Age", "function": "Minimum"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            },
+            {
+                "name": "Complex query 2",
+                "json": {
+                    "group_columns": ["Position", "Age", "Surname"],
+                    "select": [
+                        {"column": "Salary", "function": "Maximum"},
+                        {"column": "Salary", "function": "Minimum"},
+                        {"column": "Salary", "function": "Average"}
+                    ],
+                    "table_name": "medium_size_few_keys_karol"
+                }
+            }
+        ]
+    },
+    {
+        "testname": "Medium_Size_Many_Keys_Queries_1",
+        "queries": [
+            {
+                "name": "Simple query 1",
+                "json": {
+                    "group_columns": ["Surname"],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Simple query 2",
+                "json": {
+                    "group_columns": ["Countries"],
+                    "select": [
+                        {"column": "Digits", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Simple query 3",
+                "json": {
+                    "group_columns": ["Random"],
+                    "select": [
+                        {"column": "Age", "function": "Average"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Medium query 1",
+                "json": {
+                    "group_columns": ["Surname", "Countries"],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Age", "function": "Average"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Medium query 2",
+                "json": {
+                    "group_columns": ["RandomGIGA", "RandomMedium"],
+                    "select": [
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Medium query 3",
+                "json": {
+                    "group_columns": ["Kraje", "Surname"],
+                    "select": [
+                        {"column": "Age", "function": "Average"},
+                        {"column": "Digits", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Complex query 1",
+                "json": {
+                    "group_columns": ["Countries", "Surname", "Random"],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Maximum"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Complex query 2",
+                "json": {
+                    "group_columns": ["RandomGIGA", "RandomMedium"],
+                    "select": [
+                        {"column": "Digits", "function": "Maximum"},
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Kraje", "function": "Average"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            },
+            {
+                "name": "Complex query 3",
+                "json": {
+                    "group_columns": ["Surname", "Random"],
+                    "select": [
+                        {"column": "Age", "function": "Maximum"},
+                        {"column": "Digits", "function": "Average"},
+                        {"column": "Kraje", "function": "Average"}
+                    ],
+                    "table_name": "medium_size_many_keys"
+                }
+            }
+        ]
+    }
+]
