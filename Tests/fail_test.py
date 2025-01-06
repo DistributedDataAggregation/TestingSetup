face2460f5b5a773b15196f3ae26ee01901f2e29
Kapiszon <krol.kacper.pg2@gmail.com>
2025-01-06 20:39:02 +0100

    feat: add more fail tests

--- Tests/fail_test.py
+++ Tests/fail_test.py
@@ -13,7 +13,7 @@ non_existent_column_test = [
         "json": {
             "group_columns": ["NonExistentColumn"],
             "select": [
-                {"column": "NonExistentColumn", "function": "Average"}
+                {"column": "Salary", "function": "Average"}
             ],
             "table_name": "large_salary_overflow_dataset"
         }
@@ -37,9 +37,9 @@ unsupported_data_type_test = [
     {
         "name": "Unsupported Data Type",
         "json": {
-            "group_columns": ["IsEmployee"],
+            "group_columns": ["Name"],
             "select": [
-                {"column": "IsEmployee", "function": "Maximum"}
+                {"column": "Date", "function": "Maximum"}
             ],
             "table_name": "missing_values_dataset"
         }
@@ -51,10 +51,9 @@ def test_unsupported_data_type_queries(test):
     """Testuje zapytanie z nieobsługiwanym typem danych"""
     print(f"Running test: {test['name']}")
     response = requests.post(API_URL, json=test["json"])
-    print(response)
-    assert response.status_code == 500, f"Expected HTTP 500, got {response.status_code}"
+    print(response.json())
+    assert response.status_code == 200, f"Expected HTTP 500, got {response.status_code}"
     response_json = response.json()
-    print(response)
     assert "error" in response_json["result"], "Expected 'error' key in response"
     print(f"Error received: {response_json['result']['error']}")
 
@@ -86,22 +85,57 @@ def test_invalid_function_queries(test):
 
 
 
-# empty_query_test = [
-#     {
-#         "name": "Empty Query",
-#         "json": {}
-#     }
-# ]
+empty_query_test = [
+    {
+        "name": "Empty Query",
+        "json": {}
+    }
+]
 
-# @pytest.mark.parametrize("test", empty_query_test)
-# def test_empty_query(test):
-#     """Testuje zapytanie z pustym JSON"""
-#     print(f"Running test: {test['name']}")
-#     response = requests.post(API_URL, json=test["json"])
-#     assert response.status_code == 500, f"Expected HTTP 500, got {response.status_code}"
-#     response_json = response.json()
-#     assert "error" in response_json['result'], "Expected 'error' key in response"
-#     print(f"Error received: {response_json['result']['error']}")
+@pytest.mark.parametrize("test", empty_query_test)
+def test_empty_query(test):
+    """Testuje zapytanie z pustym JSON"""
+    print(f"Running test: {test['name']}")
+    response = requests.post(API_URL, json=test["json"])
+    assert response.status_code == 500, f"Expected HTTP 500, got {response.status_code}"
+    response_json = response.json()
+    assert "error" in response_json['result'], "Expected 'error' key in response"
+    print(f"Error received: {response_json['result']['error']}")
+
+duplicated_columns_test = [
+    {
+        "name": "Duplicate Column in Group and Select",
+        "json": {
+            "group_columns": ["Name","Salary","Age"],
+            "select": [
+                {"column": "Wiek", "function": "Count"},
+                {"column": "Age", "function": "Count"}
+            ],
+            "table_name": "missing_values_dataset"
+        }
+    }
+    ,{
+        "name": "Duplicate Column in Group and Select",
+        "json": {
+            "group_columns": ["Age","Age"],
+            "select": [
+                {"column": "Salary", "function": "Count"}
+            ],
+            "table_name": "missing_values_dataset"
+        }
+    },
+]
+
+@pytest.mark.parametrize("test", duplicated_columns_test)
+def test_duplicated_column_query(test):
+    """Testuje zapytanie z duplikowanymi kolumnami w JSON"""
+    print(f"Running test: {test['name']}")
+    response = requests.post(API_URL, json=test["json"])
+    assert response.status_code == 500, f"Expected HTTP 500, got {response.status_code}"
+    response_json = response.json()
+    assert "error" in response_json['result'], "Expected 'error' key in response"
+    print(f"Error received: {response_json['result']['error']}")
+
 
 non_existent_table_test = [
     {
@@ -112,6 +146,15 @@ non_existent_table_test = [
                 {"column": "Salary", "function": "Average"}
             ],
             "table_name": "karol_przystojny"
+        }
+    },
+     {
+        "name": "Non-existent Table",
+        "json": {
+            "group_columns": ["Age"],
+            "select": [
+                {"column": "Salary", "function": "Average"}
+            ],
         }
     }
 ]
@@ -127,3 +170,5 @@ def test_non_existent_table_queries(test):
     print(response)
     assert "error" in response_json["result"], "Expected 'error' key in response"
     print(f"Error received: {response_json["result"]['error']}")
+
+
